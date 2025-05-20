5518ceb98d5f5539e8cb8eb68e52b35f0f6fd520
Anurag <anuragmishra11904@gmail.com>
2025-05-20 22:13:01 +0530

    . Cyclically rotate an array by one

---
+++ ArrayPrograms/Cyclic_Rotation.java
@@ -0,0 +1,22 @@
+package ArrayPrograms;
+
+public class Cyclic_Rotation {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int LastValue =arr[arr.length-1];
+
+        for(int i=arr.length-1;i>0;i--){
+            arr[i]=arr[i-1];
+
+
+        }
+        arr[0]=LastValue;
+        System.out.print("{");
+        for(int k:arr){
+
+            System.out.print(k+ ",");
+        }
+        System.out.print("}");
+    }
+    
+}
