55a31e2ca50de20b93d44e5edb1ae1614fdebbce
Anurag <anuragmishra11904@gmail.com>
2025-05-28 09:54:07 +0530

    Check the array is sorted or not

--- ArrayPrograms/CheckArraySorted.java
+++ ArrayPrograms/CheckArraySorted.java
@@ -1,4 +1,6 @@
+package ArrayPrograms;
 
+import java.util.Scanner;
 public class CheckArraySorted {
     
         static boolean CheckArray(int []arr){
@@ -14,7 +16,20 @@ public class CheckArraySorted {
         }
        
         public static void main(String[] args){
-            int arr[]={1,2,3,4,556};
+            Scanner sc =new Scanner(System.in);
+            System.out.println("enter the size of an array");
+            int n=sc.nextInt();
+            int arr [] = new int[n];
+            System.out.println("enter the elements of the array");
+            for(int i=0;i<arr.length;i++){
+                arr[i]=sc.nextInt();
+            }
+            System.out.println("the Array according to you is given below");
+            for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+ ",");
+            }
+            // int arr[]={1,2,3,4,556};
+            System.out.println();
            boolean result= CheckArray(arr);
              System.out.println("Is given array is sorted  ->>" + result);
             
